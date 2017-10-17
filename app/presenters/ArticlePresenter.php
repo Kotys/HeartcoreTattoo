cd1fdad613882d84e20d5bf65a82288736426469
Jan Kotrba <jan.kotrba@b2a.cz>
2017-10-17 21:43:39 +0200

    Article page first step

---
+++ app/presenters/ArticlePresenter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kotycz
+ * Date: 17/10/2017
+ * Time: 20:51
+ */
+
+namespace App\Presenters;
+
+use Nette\Application\UI\Presenter;
+
+class ArticlePresenter extends Presenter
+{
+
+}
