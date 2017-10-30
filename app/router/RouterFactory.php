a7afdf6470de25d6ad376fc47fc2fe6683e7cf08
Jan Kotrba <jan.kotrba@b2a.cz>
2017-10-31 00:38:49 +0100

    Log applyRouting errors on development, throw AppException in production

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -6,7 +6,8 @@ use KotysPackages\ContentModule\Article\Service\ArticlesRouting;
 use Nette;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
-
+use Tracy\Debugger;
+use Tracy\Logger;
 
 /**
  * Class RouterFactory
@@ -19,12 +20,22 @@ class RouterFactory
 	/**
 	 * @param ArticlesRouting $articlesRoutingGenerator
 	 * @return RouteList
+	 * @throws Nette\Application\ApplicationException
 	 */
 	public static function createRouter(ArticlesRouting $articlesRoutingGenerator)
 	{
 		$appRouter = new RouteList;
 
-//		$articlesRoutingGenerator->applyRouting($appRouter);
+		try {
+			$articlesRoutingGenerator->applyRouting($appRouter);
+			// More generators
+		} catch (\Exception $e) {
+			if (Debugger::$productionMode) {
+				throw new Nette\Application\ApplicationException($e->getMessage());
+			} else {
+				Debugger::log($e, Logger::CRITICAL);
+			}
+		}
 
 		$appRouter[] = new Route('<presenter>/<action>[/<id>]', 'Article:default');
 		return $appRouter;
